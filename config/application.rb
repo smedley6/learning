3100172c9cf8929e2beeb8daa6741e3c81520257
smedley6 <roflror@gmail.com>
2019-05-23 19:41:22 +0300

    Initialized commit

---
+++ config/application.rb
@@ -0,0 +1,25 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Learning
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                        	fixtures: true,
+                        	view_spec: false,
+                        	helper_specs: false,
+                        	routing_specs: false,
+                        	request_specs: false,
+                        	controller_spec: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
+  end
+end
